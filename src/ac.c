10cb249c23d4a1f2d8b29a789964619520a18fe9
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2022-02-16 22:58:32 +0000

    now working

--- src/ac.c
+++ src/ac.c
@@ -1,34 +1,48 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <X11/extensions/XTest.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
-void click(Display *dp, int button) {
-    XEvent ev;
-    memset(&ev, 0, sizeof(ev));
-    ev.xbutton.button = button;
-    ev.xbutton.same_screen = True;
-    ev.xbutton.subwindow = DefaultRootWindow(dp);
-
-    while (ev.xbutton.subwindow) {
-        ev.xbutton.window = ev.xbutton.subwindow;
-        XQueryPointer(dp, ev.xbutton.window, &ev.xbutton.root, &ev.xbutton.subwindow, &ev.xbutton.x_root, &ev.xbutton.y_root, &ev.xbutton.x, &ev.xbutton.y, &ev.xbutton.state);
-    }
-
-    ev.type = ButtonPress;
-    XSendEvent(dp, PointerWindow, True, ButtonReleaseMask, &ev);
-    XFlush(dp);
-    usleep(1);
+void autoclick(Display *d) {
+	puts("running...");
+	sleep(2);
+	puts("stop sleep");
+	for (int i = 0; i < 50; ++i) {
+		printf("%d", i);
+		fflush(stdout);
+        	XTestFakeButtonEvent(d, Button1, True, CurrentTime);
+        	XFlush(d);
+       		XTestFakeButtonEvent(d, Button1, False, CurrentTime);
+        	XFlush(d);
+	}
+	puts("after");
 }
 
 int main(void) {
     Display *dp = XOpenDisplay(NULL);
+    unsigned int event_mask = KeyPressMask;
 
+    XGrabKey(dp, XKeysymToKeycode(dp, XK_F6), ControlMask, DefaultRootWindow(dp), True, GrabModeAsync, GrabModeAsync);
+    XSelectInput(dp, DefaultRootWindow(dp), event_mask);
+
+    XEvent ev;
     while (True) {
-        click(dp, Button1);
-        sleep(0.5);
+        while (XPending(dp)) {
+            XNextEvent(dp, &ev);
+            switch (ev.type) {
+                case KeyPress: {
+                    if (XLookupKeysym(&ev.xkey, 0) == XK_F6 && ev.xkey.state & ControlMask) {
+                        puts("started autoclicker...");
+                        autoclick(dp);
+                    }
+                    break;
+                }
+            }
+        }
     }
 
     XCloseDisplay(dp);
