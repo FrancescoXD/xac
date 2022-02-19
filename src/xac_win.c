a393fd8e4b9340c63c280822c099b879c7b37db6
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2022-02-19 19:02:28 +0000

    click on windows now works

--- src/xac_win.c
+++ src/xac_win.c
@@ -16,13 +16,13 @@ int main_w(int argc, char *argv[]) {
 	while (1){
 		INPUT input;
 		input.type = INPUT_MOUSE;
-		input.mi.dwFlags = (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE | MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP);
+		input.mi.dwFlags = (MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP);
 		input.mi.mouseData = 0;
 		input.mi.dwExtraInfo = NULL;
 		input.mi.time = 0;
 		SendInput(1, &input, sizeof(INPUT));
 		puts("clicked");
-		usleep(1000000);
+		usleep(delay);
 	}
 
 	return 0;
